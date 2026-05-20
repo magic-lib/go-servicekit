84224beaf4764279cd5b6e0360325466ad752a86
tianlin0 <tianlin0@vip.qq.com>
2026-05-20 23:27:40 +0800

    feat: add FindMatchingRoute

--- tracer/gozero_tracing.go
+++ tracer/gozero_tracing.go
@@ -2,11 +2,11 @@ package tracer
 
 import (
 	"github.com/magic-lib/go-plat-utils/utils/httputil/param"
+	"github.com/samber/lo"
 	"github.com/zeromicro/go-zero/core/service"
 	ztrace "github.com/zeromicro/go-zero/core/trace"
 	"github.com/zeromicro/go-zero/rest"
 	"net/http"
-	"strings"
 )
 
 // InitGoZeroTracing 初始化gozeroTracing,避免后续更新会被 rest.MustNewServer 抢占的问题
@@ -44,12 +44,16 @@ func (hc *TraceConfig) GoZeroMiddleware(serv *rest.Server, pc ...*param.PathConf
 	}
 	return hc.commMiddleware(func(r *http.Request) (string, string) {
 		routesList := serv.Routes()
-		for _, route := range routesList {
-			if strings.ToUpper(route.Method) == strings.ToUpper(r.Method) {
-				if param.PathMatch(r.URL.Path, route.Path, pc...) {
-					return "", defaultSpanNameFormatter(route.Method, route.Path)
-				}
-			}
+		currentRouteList := make([]*param.CurrentRoute, 0)
+		lo.ForEach(routesList, func(route rest.Route, index int) {
+			currentRouteList = append(currentRouteList, &param.CurrentRoute{
+				Method: route.Method,
+				Path:   route.Path,
+			})
+		})
+		oneRoute, ok := param.FindMatchingRoute(r, currentRouteList, pc...)
+		if ok {
+			return "", defaultSpanNameFormatter(oneRoute.Method, oneRoute.Path)
 		}
 		return "", ""
 	})
